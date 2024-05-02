e066bf8671fadd8a6bf24457cb1423e3e0903ace
John Desmond <johndesmond631@gmail.com>
2024-05-02 08:58:56 -0400

    almost done with the SRP video

--- section-1/srp.py
+++ section-1/srp.py
@@ -3,7 +3,7 @@
 class Journal:
     def __init__(self):
         self.entries = []
-        self.count  
+        self.count = 0
     
     def add_entry(self, text):
         self.count += 1
@@ -15,9 +15,38 @@ class Journal:
     def __str__(self):
         return '\n'.join(self.entries)
     
+    # more functionality that is not SR 
+    # these functions all fall into the category of: persistence.
+    # create a separate class for persistence.
+    # def save(self, filename):
+    #     file = open(filename, 'w')
+    #     file.write(str(self))
+    #     file.close()
+    
+    # def load(self, filename):
+    #     pass
+
+    # def load_from_web(self, uri):
+    #     pass
+
+
+class PersistenceManager:
+
+    @staticmethod
+    def save_to_file(journal, filename):
+        file = open(filename, 'w')
+        file.write(str(journal))
+        file.close()
+
+    
 
 j = Journal()
 j.add_entry('I cried today.')
 j.add_entry('I ate a bug.')
 print(f'Journal entries:\n{j}')
 
+filename = "journal_entry.txt"
+PersistenceManager.save_to_file(j, filename)
+
+with open(filename) as fh:
+    print(fh.read())

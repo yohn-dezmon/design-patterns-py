37679ee672c0c6d662edded4fa8b1d32f82f4bf4
John Desmond <johndesmond631@gmail.com>
2024-06-06 20:26:06 -0400

    finish lsp lecture

--- section-1/liskov_substitution_principle.py
+++ section-1/liskov_substitution_principle.py
@@ -35,10 +35,22 @@ class Square(Rectangle):
     at first we'll design this such that it breaks use_it()
     even though it's a child class of Rectangle
     """
+    def __init__(self, size):
+        Rectangle.__init__(self, size, size)
+    
+    @Rectangle.width.setter
+    def width(self, value):
+        self._width = self._height = value 
+
+    @Rectangle.height.setter
+    def height(self, value):
+        self._height = self._width = value 
+
 
 
 def use_it(rc):
     w = rc.width 
+    # this ends up changing the width as well... (for square)
     rc.height = 10
     expected = int(w * 10)
     print(f'Expected an area of {expected}, got {rc.area}')
@@ -46,4 +58,13 @@ def use_it(rc):
 rc = Rectangle(2, 3)
 use_it(rc)
 
+sq = Square(5)
+use_it(5)
 
+
+"""
+Solution?
+- don't create a square class lol
+- don't change the way getters/setters work!
+
+"""
